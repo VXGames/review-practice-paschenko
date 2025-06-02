c60ce05ff99e517abe1c8f44de816faaa6c1bccb
VXGames <daniilpasenko760@gmail.com>
2025-06-02 09:50:30 +0300

    добавлена новая функция division

--- src/main.java
+++ src/main.java
@@ -6,6 +6,7 @@ public class main {
         System.out.println("Hello World!");
         System.out.println(returnJava("file"));
         System.out.println(sum(5, 1));
+        System.out.println(division(100, 80));
     }
 
     public static String returnJava (String word) {
@@ -14,5 +15,9 @@ public class main {
 
     public static int sum (int a, int b) {
         return a + b;
+    }
+
+    public static float division (int a, int b) {
+        return (float) a / b;
     }
 }
