c4457d974f8acca98733d97c8dcb2eb84c37e25e
VXGames <daniilpasenko760@gmail.com>
2025-06-02 09:27:24 +0300

    добавление нового файла

---
+++ src/main.java
@@ -0,0 +1,14 @@
+
+
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(returnJava("file"));
+    }
+
+    //code
+    public static String returnJava (String word) {
+        return word + ".java";
+    }
+}
