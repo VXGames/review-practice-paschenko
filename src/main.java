40409e3590f5cf405e8ade8f5535cae4726155dc
VXGames <daniilpasenko760@gmail.com>
2025-06-02 09:32:35 +0300

    добавлена функция sum

--- src/main.java
+++ src/main.java
@@ -5,10 +5,14 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println(returnJava("file"));
+        System.out.println(sum(5, 1));
     }
 
-    //code
     public static String returnJava (String word) {
         return word + ".java";
+    }
+
+    public static int sum (int a, int b) {
+        return a + b;
     }
 }
